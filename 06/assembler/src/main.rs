01f181b7cba42f5120dcc7e2e10c9769413ef0a3
mohanrajendran <lynermohan@gmail.com>
2016-11-07 20:44:52 -0600

    Modified assembler to use getopts

--- 06/assembler/src/main.rs
+++ 06/assembler/src/main.rs
@@ -3,19 +3,39 @@ use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 
+extern crate getopts;
+use getopts::Options;
+use getopts::ParsingStyle;
+
 pub mod parser;
 pub mod code;
 use parser::Parser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    if args.len() != 2 {
-        println!("Usage: ./assembler FILENAME");
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "OUTFILE");
+    opts.parsing_style(ParsingStyle::FloatingFrees);
+
+    let matches = opts.parse(&args[1..]).expect("Unable to parse arguments.");
+
+    let in_file_name = if matches.free.len() == 1 {
+        matches.free[0].clone()
+    } else {
+        let brief = format!("Usage: {} FILE [options]", program);
+        println!("{}", opts.usage(&brief));
         return;
-    }
+    };
 
-    let mut in_file = File::open(&args[1]).expect("Unable to find file.");
+    let out_file_name = if matches.opt_present("o") {
+        matches.opt_str("o").unwrap()
+    } else {
+        in_file_name.replace(".asm", ".hack")
+    };
+
+    let mut in_file = File::open(in_file_name).expect("Unable to find file.");
     let mut buffer = String::new();
     in_file.read_to_string(&mut buffer).expect("Unable to read file.");
     let buffer = buffer;
@@ -24,7 +44,7 @@ fn main() {
     //parser.print();
 
     let assembly = parser.assemble();
-    let mut out_file = File::create(args[1].replace(".asm", ".mhack")).expect("Unable to create file.");
+    let mut out_file = File::create(out_file_name).expect("Unable to create file.");
     out_file.write_all(assembly.as_bytes()).expect("Unable to write to file.");
 }
 
